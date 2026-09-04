1b9147397fba24cfb821a0f1afa3c651f41eadc3
Alomgir Hossain <alomgirhosssain71@gmail.com>
2026-09-04 12:42:10 +0600

    02. Move Zeroes

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -1,6 +1,6 @@
 // 01. Contains Duplicate
 
-const containsDuplicate = function(nums) {
+const containsDuplicate = function (nums) {
     const seen = new Set();
     for (const n of nums) {
         if (seen.has(n)) return true;
@@ -8,5 +8,26 @@ const containsDuplicate = function(nums) {
     }
     return false;
 };
- 
+
 // console.log("01:", containsDuplicate([1, 2, 3, 1])); 
+
+
+// 02. Move Zeroes
+
+const moveZeroes = function (nums) {
+    let insertPos = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[insertPos] = nums[i];
+            insertPos++;
+        }
+    }
+    for (let i = insertPos; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+};
+
+// const arr02 = [0, 1, 0, 3, 12];
+// moveZeroes(arr02);
+// console.log("02:", arr02); 
+
