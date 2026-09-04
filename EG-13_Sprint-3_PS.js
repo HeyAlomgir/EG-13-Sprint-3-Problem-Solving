3df19cd2c33ee71480a72bb7bf2204433ffad28f
Alomgir Hossain <alomgirhosssain71@gmail.com>
2026-09-04 12:47:44 +0600

    05. Majority Element

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -69,3 +69,23 @@ const canConstruct = function (ransomNote, magazine) {
 };
 
 // console.log("04:", canConstruct("aa", "aab"));
+
+
+
+
+// 05. Majority Element
+
+const majorityElement = function(nums) {
+    let count = 0;
+    let candidate = null;
+    for (const n of nums) {
+        if (count === 0) {
+            candidate = n;
+        }
+        count += (n === candidate) ? 1 : -1;
+    }
+    return candidate;
+};
+ 
+// console.log("05:", majorityElement([2, 2, 1, 1, 1, 2, 2]));
+
